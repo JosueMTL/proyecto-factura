3a006c6d85a0da0e1a8a13e72af23786fde2dc46
Josue MTL <116099684+JosueMTL@users.noreply.github.com>
2024-01-14 20:43:15 -0500

    commit

--- src/main/kotlin/com/trade/billing/dto/ProductDto.kt
+++ src/main/kotlin/com/trade/billing/dto/ProductDto.kt
@@ -4,3 +4,10 @@ class ProductDto(
     var id:Long?,
     var descriptionBrand: String?=null
 )
+class LoginDto {
+    var username:String?=null
+    var password:String?=null
+}
+class TokenDto {
+    var jwt:String?=null
+}
---
+++ src/main/kotlin/com/trade/billing/repository/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.trade.billing.repository
+
+import com.trade.billing.model.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository {
+    interface UserRepository: JpaRepository<UserEntity, String> {
+        fun findByUsername(username: String): UserEntity?
+    }
+}
